4a6e4aaafa40e2b84d0ef3036a1e7804be2de3be
Guz117 <cristian.guzman117@gmail.com>
2022-01-19 11:42:19 +0100

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+      <h1>BOOLFLIX</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss">
+  .header {
+    background-color: black;
+      h1 {
+        color: red;
+      }
+  }
+
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      ciao
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style lang="scss">
+
+</style>
